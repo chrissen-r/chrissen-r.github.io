06e7344b26bcedd2f2a568f2dbfdaea81e4f1df2
LouisJackson <louis.amiot92@gmail.com>
2016-04-10 23:51:05 +0200

    Home

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var App = function() {
+	this.init();
+};
+
+App.prototype.init = function() {
+	
+	this.$ = {};
+	this.$.body = $('body');
+
+	this.initPage();
+};
+
+App.prototype.initPage = function() {
+
+};
