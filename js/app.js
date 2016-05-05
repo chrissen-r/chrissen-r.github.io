17c491f41a12bf1ca1fc4c6c14c6cc69e6102a15
LouisJackson <louis.amiot92@gmail.com>
2016-05-05 13:52:36 +0200

    Changes in animation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var App = function() {
+
 	this.init();
 };
 
@@ -7,14 +8,47 @@ App.prototype.init = function() {
 	this.$ = {};
 	this.$.body = $('body');
 
+	this.$.loader = this.$.body.find('.loader');
+	this.$.loaded = this.$.body.find('.loader .loaded');
+
+	this.load();
+
 	this.templates = window.templates;
 
-	console.log(this.templates);
 
-	this.setRouting();
-	this.initProjectEvents();
 
 };
+
+App.prototype.load = function() {
+
+	var that = this;
+
+	var img_to_load = [ '/img/explore.jpg', '/img/layer.jpg',  '/img/masterpiece.jpg',  '/img/tesla.jpg',   '/img/louis.jpg'];
+	var loaded_images = 0;
+
+	for (var i=0; i<img_to_load.length; i++) {
+	    var img = document.createElement('img');
+	    img.src = img_to_load[i];
+	    img.style.display = 'hidden'; // don't display preloaded images
+	    img.onload = function () {
+	        loaded_images++;
+
+	        if (loaded_images == img_to_load.length-1) {
+	        	var percent = 100*loaded_images/(img_to_load.length-1);
+	        	that.$.loaded.width(percent+'%');
+	            that.$.loader.fadeOut('slow');
+	            that.setRouting();
+				that.initProjectEvents();
+	        }
+	        else {
+	        	var percent = 100*loaded_images/(img_to_load.length-1);
+	        	that.$.loaded.width(percent+'%');
+	        }
+	    }
+	    document.body.appendChild(img);
+	}
+
+}
 
 App.prototype.initPage = function() {
 	var view = $('.view');
@@ -41,7 +75,7 @@ App.prototype.initProjectEvents = function() {
 
 App.prototype.setRouting = function() {
 
-	this.router = new Navigo();
+	this.router = new Navigo(null,false);
 	var that = this;
 
 	this.router.on('/contact', function() {
@@ -66,7 +100,7 @@ App.prototype.loadView = function(view,params) {
 
 	if (oldView.length > 0) {
 		this.animateOut(oldView,view,params);
-		this.animateIn($('.view:nth-child(2)'),true);
+		this.animateIn($('.view:nth-child(2)'),true,oldView);
 	}
 	else {
 		$('#app').html(this.getTemplate(view,params));
@@ -102,13 +136,17 @@ App.prototype.changeProject = function(direction) {
 	}
 };
 
-App.prototype.animateIn = function(elem,wait) {
+App.prototype.animateIn = function(elem,wait,oldView) {
 
 	var newView = elem;
+	if (oldView.hasClass('project')) {
+		newView.addClass('from-project');
+	}
 	newView.addClass('animate-in');
 	
 	if (wait) {
 		setTimeout(function() {
+			newView.find('.top, .bottom').fadeIn('slow');
 			setTimeout(function() {
 				newView.removeClass('animate-in');
 			},50);
@@ -119,6 +157,7 @@ App.prototype.animateIn = function(elem,wait) {
 	}
 	else {
 		setTimeout(function() {
+			newView.find('.top, .bottom').fadeIn('slow');
 			newView.removeClass('animate-in');
 		},50);
 		setTimeout(function() {
@@ -126,14 +165,15 @@ App.prototype.animateIn = function(elem,wait) {
 		},900);
 	}
 
+	this.router.updatePageLinks();
+
 };
 
 App.prototype.animateOut = function(elem,view,params) {
 
 	var oldView = elem;
 
-	console.log('here');
-
+	oldView.find('.top, .bottom').fadeOut();
 	oldView.find('.content-centered').addClass('scale-out');
 	oldView.css('zIndex', '7');
 	oldView.after(this.getTemplate(view,params));
